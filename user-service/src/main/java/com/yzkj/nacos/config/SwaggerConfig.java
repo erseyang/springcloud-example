69992b8fd5f7008908c9d36aa888b7a6e159d76e
EricYang <erseyang@gmail.com>
2024-12-17 23:50:44 +0800

    添加swagger配置。

--- user-service/src/main/java/com/yzkj/nacos/config/SwaggerConfig.java
+++ user-service/src/main/java/com/yzkj/nacos/config/SwaggerConfig.java
@@ -1,4 +1,29 @@
 package com.yzkj.nacos.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Value("${springdoc.api-docs.enabled:true}")
+    private boolean enabled;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        OpenAPI openAPI = new OpenAPI();
+        if (enabled) {
+            Info info = new Info();
+            info.setTitle("Demo");
+            info.setVersion("1.0.0");
+            info.setDescription("Demo测试");
+            openAPI.setInfo(info);
+        } else {
+            openAPI.setPaths(null);
+        }
+        return openAPI;
+    }
 }
